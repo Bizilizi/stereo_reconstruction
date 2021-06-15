a457146ea20b86fccfdc5d2759c3a251cb9a3ca4
timsloo <slooten.tim@gmail.com>
2021-06-15 16:10:32 +0200

    WIP:Adding CMakeLists and Eigen depencendies

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,12 @@
 
+#include <iostream>
+#include <opencv4/opencv2/opencv.hpp>
+#include "Eigen.h"
 
 int main()
 {
-
+    std::cout << "Hello world! " << std::endl;
+    cv::Mat image;
+    MatrixXf testMat;
     return 0;
 }
