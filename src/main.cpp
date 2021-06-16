14bfa34f9e52ac8f787e2bdbcb248fe7e2c50414
timsloo <slooten.tim@gmail.com>
2021-06-16 10:09:46 +0200

    CMake fix for Eigen

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,13 @@
 
 #include <iostream>
-#include <opencv4/opencv2/opencv.hpp>
+//#include <opencv4/opencv2/opencv.hpp>
 #include "Eigen.h"
 
 int main()
 {
     std::cout << "Hello world! " << std::endl;
-    cv::Mat image;
-    MatrixXf testMat;
+    //cv::Mat image;
+    MatrixXf testMat = Matrix3f::Zero();
+    std::cout << testMat;
     return 0;
 }
--- src/test_loading.cpp
+++ src/test_loading.cpp
@@ -3,7 +3,7 @@
 //
 
 #include <stdio.h>
-#include <opencv2/opencv.hpp>
+#include <opencv4/opencv2/opencv.hpp>
 
 
 using namespace cv;
@@ -21,7 +21,7 @@ int main(int argc, char** argv )
         printf("No image data \n");
         return -1;
     }
-    namedWindow("Display Image", WINDOW_AUTOSIZE );
+    //namedWindow("Display Image", WINDOW_AUTOSIZE );
     imshow("Display Image", image);
     waitKey(0);
     return 0;
