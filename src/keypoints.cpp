57a66f701e4b487f3b9efcc27953ba1103c8b3d2
timsloo <slooten.tim@gmail.com>
2021-06-19 14:31:05 +0200

    Feature extraction and matching working - Ransac outlier filtering tbd

--- src/keypoints.cpp
+++ src/keypoints.cpp
@@ -7,36 +7,75 @@
 #include <opencv2/core.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/features2d.hpp>
-//#include <opencv2/xfeatures2d.hpp>
+
+
+void featureMatching(const cv::Mat& featuresLeft, const cv::Mat& featuresRight, std::vector<cv::DMatch>& outputMatches, float ratio_thresh=0.7f) {
+    cv::Ptr<cv::DescriptorMatcher> matcher = cv::DescriptorMatcher::create(cv::DescriptorMatcher::FLANNBASED);
+    std::vector< std::vector<cv::DMatch> > knn_matches;
+    matcher->knnMatch(featuresLeft, featuresRight, knn_matches, 2 );
+
+    // filter matches using the Lowe's ratio test
+    for (auto & knn_match : knn_matches)
+    {
+        if (knn_match[0].distance < ratio_thresh * knn_match[1].distance)
+        {
+            outputMatches.push_back(knn_match[0]);
+        }
+    }
+}
+
+
+void SIFTKeypointDetection(const cv::Mat &image, std::vector<cv::KeyPoint> &outputKeypoints, cv::Mat& outputDescriptor,
+                           const float edgeThreshold = 10, const float contrastThreshold = 0.04){
+    // detect outputKeypoints using SIFT
+    cv::Ptr<cv::SIFT> detector = cv::SIFT::create(100, 3, contrastThreshold, edgeThreshold);
+    detector->detectAndCompute(image, cv::noArray(), outputKeypoints, outputDescriptor);
+
+    // sort detected outputKeypoints
+    // std::sort(outputKeypoints.begin(), outputKeypoints.end(), [](cv::KeyPoint& p1, cv::KeyPoint& p2) {return p1.pt.x > p2.pt.x; });
+
+    // erase duplicates
+    // outputKeypoints.erase(std::unique(outputKeypoints.begin(), outputKeypoints.end(), [](cv::KeyPoint& p1, cv::KeyPoint& p2) {
+    //     return std::hypot(p1.pt.x-p2.pt.x, p1.pt.y - p2.pt.y) < 10; }), outputKeypoints.end());
+}
 
 
 int main(int argc, char** argv) {
     std::string image_path;
     if (argc == 1) {
         image_path = "/home/tim/repos/stereo_reconstruction/data/MiddEval3/trainingH/ArtL";
-        std::cout << "Its me! " << std::endl;
     } else {
         image_path = std::string(argv[1]);
     }
 
-    // Stereo Images
-    cv::Mat image1 = cv::imread(image_path + "/im0.png", cv::IMREAD_COLOR);
+    // load stereo images
+    cv::Mat imageLeft = cv::imread(image_path + "/im0.png", cv::IMREAD_COLOR);
+    cv::Mat imageRight = cv::imread(image_path + "/im1.png", cv::IMREAD_COLOR);
 
-    // SIFT feature
-    //cv::SiftFeatureDetector detector;
-    //std::vector<cv::KeyPoint> keypoints;
-    //detector.detect(image1, keypoints);
+    // find keypoints
+    std::vector<cv::KeyPoint> keypointsLeft, keypointsRight;
+    cv::Mat featuresLeft, featuresRight;
+    SIFTKeypointDetection(imageLeft, keypointsLeft, featuresLeft);
+    SIFTKeypointDetection(imageRight, keypointsRight, featuresRight);
 
-    //
-    int featuresToRetain = 20;
-    cv::Ptr<cv::SIFT> detector = cv::SIFT::create(featuresToRetain);
-    std::vector<cv::KeyPoint> keypoints;
-    detector->detect(image1, keypoints);
+    // find correspondences
+    std::vector<cv::DMatch> matches;
+    featureMatching(featuresLeft, featuresRight, matches);
 
-    // Add results to image and save.
-    cv::Mat outputImage;
-    cv::drawKeypoints(image1, keypoints, outputImage);
-    cv::imwrite("../../results/out.png", outputImage);
+    // draw matches
+    cv::Mat img_matches;
+    drawMatches( imageLeft, keypointsLeft, imageRight, keypointsRight, matches, img_matches, cv::Scalar::all(-1),
+                 cv::Scalar::all(-1), std::vector<char>(), cv::DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS );
+
+    // visualization for feature extraction
+    cv::Mat outputImageLeft, outputImageRight;
+    cv::drawKeypoints(imageLeft, keypointsLeft, outputImageLeft);
+    cv::drawKeypoints(imageRight, keypointsRight, outputImageRight);
+    cv::imwrite("../../results/imageLeft.png", outputImageLeft);
+    cv::imwrite("../../results/imageRight.png", outputImageRight);
+
+    // visualization for feature matching
+    cv::imwrite("../../results/matching_flann.png", img_matches);
 
     return 0;
 }
