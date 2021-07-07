86d229cb170c00eb616e7655eb455952abdceb33
timsloo <slooten.tim@gmail.com>
2021-07-07 10:22:41 +0200

    Initial commit for data loader

---
+++ src/data_loader.cpp
@@ -0,0 +1,7 @@
+//
+// Created by tim on 07.07.21.
+//
+
+#include "data_loader.h"
+
+
---
+++ src/data_loader.h
@@ -0,0 +1,23 @@
+//
+// Created by tim on 07.07.21.
+//
+
+#ifndef STEREO_RECONSTRUCTION_DATALOADER_H
+#define STEREO_RECONSTRUCTION_DATALOADER_H
+
+
+class DataLoader {
+/**
+ * Loads data from the middlebury dataset
+ */
+public:
+    DataLoader();
+
+private:
+    std::string relativeScenarioPath;
+
+
+};
+
+
+#endif //STEREO_RECONSTRUCTION_DATALOADER_H
